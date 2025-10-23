538a9e8b9d798295da50e61dd0430e25aef75594
sathvikvelapaka <161377964+sathvikvelapaka@users.noreply.github.com>
2025-10-24 02:31:39 +0530

    Create 560-SubarraySumEqualsK

---
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -0,0 +1,22 @@
+// Last updated: 24/10/2025, 02:31:38
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum=0;
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            sum=0;
+            for(int j=i;j<nums.size();j++)
+            {
+                sum=sum+nums[j];
+                if(sum==k)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+        
+    }
+};
