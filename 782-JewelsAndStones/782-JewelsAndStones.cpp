5259ce454fab60d062ece89fcd5d3d00c3ebebaa
sathvikvelapaka <161377964+sathvikvelapaka@users.noreply.github.com>
2025-10-24 02:31:36 +0530

    Create 782-JewelsAndStones

---
+++ 782-JewelsAndStones/782-JewelsAndStones.cpp
@@ -0,0 +1,18 @@
+// Last updated: 24/10/2025, 02:31:35
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(int i=0;i<jewels.size();i++)
+        {
+            for(int j=0;j<stones.size();j++)
+            {
+                if(jewels[i]==stones[j])
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
