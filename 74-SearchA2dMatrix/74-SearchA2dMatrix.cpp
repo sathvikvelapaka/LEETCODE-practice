6fb58cdac7e7824d2197fecebf08c126d8ea928b
sathvikvelapaka <161377964+sathvikvelapaka@users.noreply.github.com>
2025-10-24 02:32:24 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.cpp
@@ -0,0 +1,18 @@
+// Last updated: 24/10/2025, 02:32:23
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
