320b4aa26e7dbc4a52a49b86303c49f71c569a57
sathvikvelapaka <161377964+sathvikvelapaka@users.noreply.github.com>
2025-10-24 02:30:32 +0530

    Create 3830-FindClosestPerson

---
+++ 3830-FindClosestPerson/3830-FindClosestPerson.cpp
@@ -0,0 +1,21 @@
+// Last updated: 24/10/2025, 02:30:32
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a=abs(x-z);
+        int b=abs(y-z);
+        if(a==b)
+        {
+            return 0;
+        }
+        else if(a>b)
+        {
+            return 2;
+        }
+        else
+        {
+            return 1;
+        }
+        
+    }
+};
