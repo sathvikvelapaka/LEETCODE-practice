d2032523d0326bf5eb8b3f2bab0d9d5a9018faa9
sathvikvelapaka <161377964+sathvikvelapaka@users.noreply.github.com>
2025-10-24 02:30:59 +0530

    Create 2491-SmallestEvenMultiple

---
+++ 2491-SmallestEvenMultiple/2491-SmallestEvenMultiple.cpp
@@ -0,0 +1,9 @@
+// Last updated: 24/10/2025, 02:30:58
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        if(n%2==0)
+        return n;
+        else return n*2;        
+    }
+};
